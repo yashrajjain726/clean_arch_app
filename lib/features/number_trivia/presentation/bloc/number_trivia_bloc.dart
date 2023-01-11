40cd633d2d2b75f89dd3bb0e6fb359303a4fafb1
Yashraj Jain <yashrajjain726@gmail.com>
2023-01-11 12:56:00 +0530

    state added

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,11 +1,12 @@
 import 'package:bloc/bloc.dart';
+import 'package:clean_arch_app/features/number_trivia/domain/entities/numer_trivia.dart';
 import 'package:equatable/equatable.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
+  NumberTriviaBloc() : super(Empty()) {
     on<NumberTriviaEvent>((event, emit) {});
   }
 }
--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -7,4 +7,26 @@ abstract class NumberTriviaState extends Equatable {
   List<Object> get props => [];
 }
 
-class NumberTriviaInitial extends NumberTriviaState {}
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia numberTrivia;
+  Loaded({
+    required this.numberTrivia,
+  });
+
+  @override
+  List<Object> get props => [numberTrivia];
+}
+
+class Error extends NumberTriviaState {
+  final String errorMsg;
+  Error({
+    required this.errorMsg,
+  });
+
+  @override
+  List<Object> get props => [errorMsg];
+}
